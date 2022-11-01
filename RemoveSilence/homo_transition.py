4a23f01b21ddcde4472f38252dc0505f0b79c663
Delbert Yip <delbert124@gmail.com>
2022-10-31 17:44:43 -0700

    Add working ffmpeg command for adding a predefined preview to the beginning of a clip and crossfading it with the rest of the video.

--- RemoveSilence/homo_transition.py
+++ RemoveSilence/homo_transition.py
@@ -1,91 +1,130 @@
-from pathlib import Path 
+from pathlib import Path
 # from RemoveSilence.common import get_filename
 from common import get_filename
-from pathlib import Path 
-from subprocess import Popen, PIPE 
+from pathlib import Path
+from subprocess import Popen, PIPE
 
 VIDEODIR = Path(r"C:/Users/delbe/Videos/subtitles/full_raw/")
-CMD = "ffmpeg --hide_banner -i {input} -an -filter_complex \"{filter}\" {map} \"{output}\""
+
+
+def run_powershell(cmd: str, stdout=PIPE, stderr=PIPE) -> Popen:
+	return Popen(['powershell.exe', cmd], stdout=PIPE, stderr=PIPE)
+
 
 def tup2secs(tup: tuple[int]) -> float:
-    h, m, s, ms = tup 
-    return 3600*h + 60*m + s + ms/1e3 
+	"""Convert a tuple of (hours, minutes, seconds, milliseconds) to total seconds"""
+	h, m, s, ms = tup
+	return 3600*h + 60*m + s + ms/1e2
+
 
 def get_preview_secs(tup: tuple[int]) -> tuple[float]:
-    return tuple(map(tup2secs, preview))
+	return tuple(map(tup2secs, tup))
+
 
 def extract_ps_output(cmd: str) -> str:
-    return Popen(['powershell.exe', cmd], stdout=PIPE).\
-        communicate()[0].\
-        decode('utf-8').\
-        strip().\
-        split('\n')[-1]
+	"""Extract the last line in `stdout` for a powershell command"""
+	return run_powershell(cmd).\
+		communicate()[0].\
+		decode('utf-8').\
+		strip().\
+		split('\n')[-1]
+
 
 def get_video_format(fp: Path) -> str:
-    cmd = f"ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 \"{fp}\""
-    fmt = extract_ps_output(cmd)
-    return fmt 
+	"""Get format of a video using powershell"""
+	cmd = f"ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 \"{fp}\""
+	fmt = extract_ps_output(cmd)
+	return fmt
+
 
 def get_video_duration(fp: Path) -> float:
-    cmd = f"ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {fp}"
-    dur = extract_ps_output(cmd)
-    return float(dur)
+	"""Get duration of a video in seconds using powershell"""
+	cmd = f"ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {fp}"
+	dur = extract_ps_output(cmd)
+	return float(dur)
 
-in_file = get_filename(
-    init_dir=VIDEODIR, 
-    title="Select input file",
-    filetypes=(("MP4 Files", "*.mp4"), )
-)
+def format_clip_times(
+	times: tuple[float, float], 
+	xdur: float, 
+	padfrac: float, 
+	padsecs: float,
+	totaldur: float) -> tuple[float, float]:
+	
+	tA, tB = times 
+	pad = max(padfrac*xdur, padsecs)
 
-in_dur = get_video_duration(in_file)
+	if round(tB - tA, ndigits=1) < xdur:
+		tB += xdur/2
 
-preview = ((0, 5, 12, 83), (0, 5, 14, 93))
+	while (tB - tA) < xdur:
+		tB += xdur/4
 
-s = get_preview_secs(preview)
+	tA, tB = map(
+		lambda x: round(x, ndigits=3),
+		(tA, tB)
+	)
 
+	if tA > pad:
+		tA -= pad 
+	if tB + pad <= totaldur:
+		tB += pad 
+	
+	return tA, tB 
 
 def build_filter(
-    preview_durs: tuple[tuple[int]], 
-    xdur: float = 1.0,
-    fmt: str) -> str:
+		preview: list[tuple[tuple[int]]],
+		xdur: float = 1.0,
+		xpad: float = 0.1,
+		pfrac: float = 0.1,
+		fadetype: str="fade",
+		afade_curvetypes: tuple[str, str] = ('exp', 'exp'),) -> str:
 
-    tA, tB = get_preview_secs(preview_durs)
-    
-    if round(tB - tA, ndigits=1) < xdur:
-        tB += xdur/2 
-    
-    while (tB - tA) < xdur:
-        tB += xdur/4
+	# curve types for audio cross fade
+	# examples: http://underpop.online.fr/f/ffmpeg/help/afade.htm.gz
+	# common types: tri (linear, default), exp, [q/h/e]sin, cub, squ, cbr, log
+	ac1, ac2 = afade_curvetypes
 
-    tA, tB = map(
-        lambda x: round(x, ndigits=3), 
-        (tA, tB)
-    )
+	# create video and audio segments that will be crossfaded
+	src = f"""
+	[0:v]trim=start={tA}:end={tB},setpts=PTS-STARTPTS[1v];
+	[0:v]trim=start=0,setpts=PTS-STARTPTS[2v];
+	[0:a]atrim=start={tA}:end={tB},asetpts=PTS-STARTPTS[1a];
+	[0:a]atrim=start=0,asetpts=PTS-STARTPTS[2a];
+	"""
+	# calculating offset: https://stackoverflow.com/a/63570355
+	offset = tB-tA-xdur
+	xfade = f"""
+	[1v][2v]xfade=transition={fadetype}:duration={xdur}:offset={offset};
+	[1a][2a]acrossfade=d={xdur}:c1={ac1}:c2={ac2}
+	"""
+	
+	return f"\"{src}\n{xfade}\""
 
-    f"""
-    [0:v]trim=start={tA}:end={tB-xdur},setpts=PTS-STARTPTS[1v];
-    [0:v]trim=start={xdur},setpts=PTS-STARTPTS[2v];
-    [0:v]trim=start={tB-xdur}:end={tB},setpts=PTS-STARTPTS[fadeoutsrc];
-    [0:v]trim=start=0:end={xdur},setpts=PTS-STARTPTS[fadeinsrc];
-    [fadeinsrc]format={fmt},
-            fade=t=in:st=0:d={xdur}:alpha=1[fadein];
-    [fadeoutsrc]format={fmt},
-            fade=t=out:st=0:d={xdur}:alpha=1[fadeout];
-    [fadein]fifo[fadeinfifo];
-    [fadeout]fifo[fadeoutfifo];
-    [fadeoutfifo][fadeinfifo]overlay[crossfade];
-    [1v][crossfade][2v]concat=n=3[output];
-    [0:a] acrossfade=d=1 [audio]
-    """
 
-    # TODO: ADD `ATRIM` for audio trimmming; then, similar to `concat` (e.g. `remove_silence.jl`), we also need to map the audio streams
+def cross_fade(
+		fpath: Path,
+		preview: tuple[tuple[int]],
+		crossfade_dur: float = 1.0,
+		**xfade_kwargs) -> None:
 
-    tmp.format(start=tA, end=tB-xdur, i=1)
-    tmp.format(start=xdur)
+	fmt = get_video_format(fpath)
+	tot = get_video_duration(fpath)
 
-[0:v]trim=start=0:end=9,setpts=PTS-STARTPTS[firstclip];
+	filter = build_filter(
+		preview, fmt,
+		xdur=crossfade_dur,
+		**xfade_kwargs
+	)
 
-[0:v]trim=start=0:end=9,setpts=PTS-STARTPTS[firstclip];
-[1:v]trim=start=1,setpts=PTS-STARTPTS[secondclip];
-[0:v]trim=start=9:end=10,setpts=PTS-STARTPTS[fadeoutsrc];
-[1:v]trim=start=0:end=1,setpts=PTS-STARTPTS[fadeinsrc];
+	outpath = fpath.parent / f"{fpath.stem}_xfade{fpath.suffix}"
+	cmd = f"ffmpeg -i \"{fpath}\" -filter_complex {filter} {outpath}"
+	run_powershell(cmd)
+
+
+def test():
+	fpath = Path(
+		r'C:/Users/delbe/Videos/subtitles/full_raw/uruha_wanchan_concat.mp4')
+	preview = ((0, 5, 12, 83), (0, 5, 14, 93))
+	cmd = cross_fade(fpath, preview)
+	return cmd
+
